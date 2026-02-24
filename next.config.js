c30361f81a63a257bdc2a92aca40d0cfcf3b331b
Daniel Levi <levidanielchinwendu@gmail.com>
2026-02-24 12:50:23 +0100

    Fix Vercel middleware nft tracing by setting outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracing: false,
+  outputFileTracing: true,
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
 }
 
 module.exports = nextConfig
